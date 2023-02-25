f3e33c81e6460378ed19e7d10576a66e6fe57036
HITidealab <121506389+HITidealab@users.noreply.github.com>
2023-02-25 19:20:01 +0530

    sdasa

--- pages/Registration.js
+++ pages/Registration.js
@@ -8,9 +8,64 @@ import Head from 'next/head'
 const Registration = () => {
   return (
     <>
-    <Head>
-      Query
-    </Head>
+   function Registration() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: handle form submission
+  };
+
+  return (
+    <div>
+      <h1>Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
     <div className='flex flex-col items-center overflow-x-hidden'>
    <Header/>
    <Navbar/>
